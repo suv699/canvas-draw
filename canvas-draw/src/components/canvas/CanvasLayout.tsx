3eb4b7efc05dc7de5c804e15094d9f2b9d19c998
Nikolai Sheryagin <suvorovite699@mail.ru>
2023-11-09 15:41:34 +0300

    fix init draw

--- canvas-draw/src/components/canvas/CanvasLayout.tsx
+++ canvas-draw/src/components/canvas/CanvasLayout.tsx
@@ -2,18 +2,22 @@ import React, { useRef, useEffect } from 'react';
 
 const CanvasLayout = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const el = canvasRef?.current;
-  const ctx = el?.getContext('2d');
-
-  const reset = () => {
-    ctx?.clearRect(0, 0, el?.width ?? 0, el?.height ?? 0);
-  };
+  const resetRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
+    const el = canvasRef?.current;
+    const ctx = el?.getContext('2d');
+
     const onMoveHandler = (e: MouseEvent) => {
       const { offsetX, offsetY } = e;
       ctx?.lineTo(offsetX, offsetY);
       ctx?.stroke();
+    };
+
+    const reset = () => {
+      ctx?.clearRect(0, 0, el?.width ?? 0, el?.height ?? 0);
+      ctx?.closePath();
+      ctx?.beginPath();
     };
 
     if (ctx) {
@@ -22,14 +26,17 @@ const CanvasLayout = () => {
       ctx.moveTo(0, 0);
     }
 
+    const btn = resetRef.current;
+    btn?.addEventListener('click', reset);
+
     el?.addEventListener('mousemove', onMoveHandler);
-    return () => el?.removeEventListener('mousemove', onMoveHandler);
+    return () => el?.removeEventListener('mousemove', onMoveHandler) && btn?.removeEventListener('click', reset);
   }, []);
 
   return (
     <div>
       <div>
-        <button className="reset-btn" onClick={reset}>
+        <button ref={resetRef} className="reset-btn">
           Reset
         </button>
       </div>
